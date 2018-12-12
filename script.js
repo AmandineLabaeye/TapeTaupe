1e90ce3feff000109c8e140dc1763eee185f62b9
Amandine <amandine.labaeye@orange.fr>
2018-12-12 13:23:36 +0100

    c

--- script.js
+++ script.js
@@ -2,7 +2,115 @@
  * Created by sstienface on 03/12/2018.
  */
 
+let Seconde = "00";
 
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+let Times;
+
+let HoleAlea;
+
+let TempsAlea;
+
+let Score = 0;
+
+let TrouRdm;
+
+$("#resetGame").hide();
+
+$(".game").hide();
+
+// Trou
+const holes = $(".hole"); // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+
+// Score
+const scoreBoard = $(".score"); //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+
+// Taupes
+const moles = $(".mole"); // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+
+function Timer() {
+
+    Seconde++;
+
+    Times = setTimeout(Timer, 1000);
+
+    $("#Temps").html(Seconde + " secondes");
+
+    if (Seconde === 10) {
+
+        clearTimeout(Times);
+
+        $(".game").fadeOut();
+
+        $("#resetGame").fadeIn();
+
+        holes.eq(HoleAlea).removeClass("up");
+
+    }
+
+}
+
+function startGame() {
+
+    Timer();
+
+    $("#startGame").fadeOut();
+
+    TrouRandom();
+
+    $(".game").fadeIn();
+
+}
+
+$("#startGame").click(function () {
+
+    startGame();
+
+});
+
+function TrouRandom() {
+
+    HoleAlea = Math.floor(Math.random() * 6);
+
+    holes.eq(HoleAlea).addClass("up");
+
+}
+
+moles.click(function () {
+
+    TempsAlea = Math.floor(Math.random() * 800 + 200);
+
+    Score++;
+
+    scoreBoard.html(Score);
+
+    Remove();
+
+    TrouRdm = setTimeout(TrouRandom, TempsAlea);
+
+});
+
+function Remove() {
+
+    holes.eq(HoleAlea).removeClass("up");
+
+}
+
+function Reset() {
+
+    $("#resetGame").slideUp();
+
+    $("#startGame").slideDown();
+
+    Score = 0;
+
+    scoreBoard.html("0");
+
+    Seconde = "00";
+
+}
+
+$("#resetGame").click(function () {
+
+    Reset();
+
+});
